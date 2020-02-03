9bf0c7da0e466ef884b509ba68927e07acdbb094
ndebianchi <ndebianchi@outlook.com>
2020-02-03 17:06:49 -0300

    add meuGrupo.js

---
+++ meuGrupo.js
@@ -0,0 +1,26 @@
+let nicolas = {
+    nome: 'Nicolas',
+    passatempo: 'Magic',
+    esporte: 'Formula 1',
+    membrosDoGrupo: ['Nicolas', 'Aguiaia', 'Ana', 'David' ],
+};
+
+let nomeAluno = {
+    nome: "Agnelo David Fico Neto",
+    passaTempo : "Tocar Violão",
+    membrosGrupo: []
+}
+
+const pessoa = {
+    Nome: "Ana Carolina",
+    Passatempo: "leitura",
+    Esporte: "Futebol",
+    MembrosDoGrupo:["Aguiaiá","Ana Carolina","Agnelo","Nicolas"]
+}
+
+let eu = {
+    nome: "Aguiaiá Akemi",
+    passatempo: "ler",
+    esporte: "caminhada",
+    membrosDoGrupo: ["Nicolas", "Aguiaia", "Ana Carolina", "David"]
+}
